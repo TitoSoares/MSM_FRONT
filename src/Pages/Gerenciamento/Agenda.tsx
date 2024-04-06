1f93e6571a786035de38076cf2485ee502a6f89e
Kristopher <kristopher.scsantos@senacsp.edu.br>
2024-04-05 21:39:44 -0300

    fav icon

--- src/Pages/Gerenciamento/Agenda.tsx
+++ src/Pages/Gerenciamento/Agenda.tsx
@@ -8,52 +8,36 @@ import { UsuarioLogadoContext } from "../../contexts/contextAuth";
 function Agenda(){
 
     const UsuarioLogadoCtx = useContext(UsuarioLogadoContext);
-    // const [events, setEvents] = useState([]);
-    // const [eventInput, setEventInput] = useState('');
-  
-    // const handleInputChange = (e) => {
-    //   setEventInput(e.target.value);
-    // };
-  
-    // const handleAddEvent = (e) => {
-    //   e.preventDefault();
-    //   if (eventInput.trim() !== '') {
-    //     setEvents([...events, eventInput]);
-    //     setEventInput('');
-    //   } else {
-    //     alert('Por favor, insira um evento válido.');
-    //   }
-    // };
+
+
 
     return(
         <div className="DivTela">
             <FuncaoCabecalho/>
-            {UsuarioLogadoCtx?.idusuario}
+            {/* {UsuarioLogadoCtx?.idusuario} */}
             
-            {/* <div className="bodyuuu">
+            <div className="bodyy">
                  <BarraLateral/>
                 <div className="container">
                     <h1>Agenda</h1>
 
                     <h2>Histórico de Eventos</h2>
                     <ul className="event-list">
-                        {events.map((event, index) => (
+                        {/* {events.map((event, ix) => (
                         <li key={index}>{event}</li>
-                        ))}
+                        ))} */}
                     </ul>
 
                     <h2>Adicionar Evento</h2>
-                    <form onSubmit={handleAddEvent}>
+                    <form>
                         <input
                         type="text"
-                        value={eventInput}
-                        onChange={handleInputChange}
                         placeholder="Digite seu evento..."
                         />
                         <input type="submit" value="Adicionar Evento" />
                     </form>
                 </div>
-            </div> */}
+            </div>
         </div>
     )
 }
